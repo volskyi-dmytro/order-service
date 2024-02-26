93da658627374a05c042803e225cd0ce0a02e3bb
volskyi-dmytro <volskyi.dmytro@gmail.com>
2024-02-26 22:48:57 +0200

    Added reactive repository interface

---
+++ src/main/java/com/uabookshop/orderservice/order/domain/OrderRepository.java
@@ -0,0 +1,6 @@
+package com.uabookshop.orderservice.order.domain;
+
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+
+public interface OrderRepository
+        extends ReactiveCrudRepository<Order, Long> {}
