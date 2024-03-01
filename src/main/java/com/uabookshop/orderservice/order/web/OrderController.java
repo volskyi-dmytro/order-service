dd6fb5f6e256952e89d6146ef994238094bfa060
volskyi-dmytro <volskyi.dmytro@gmail.com>
2024-03-01 14:51:44 +0200

    Basic implementation of Order Service

---
+++ src/main/java/com/uabookshop/orderservice/order/web/OrderController.java
@@ -0,0 +1,29 @@
+package com.uabookshop.orderservice.order.web;
+
+import com.uabookshop.orderservice.order.domain.Order;
+import com.uabookshop.orderservice.order.domain.OrderService;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("orders")
+public class OrderController {
+
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @GetMapping
+    public Flux<Order> getAllOrders() {
+        return orderService.getAllOrders();
+    }
+
+    @PostMapping
+    public Mono<Order> submitOrder(@RequestBody @Valid OrderRequest orderRequest) {
+        return orderService.submitOrder(orderRequest.isbn(), orderRequest.quantity());
+    }
+}
