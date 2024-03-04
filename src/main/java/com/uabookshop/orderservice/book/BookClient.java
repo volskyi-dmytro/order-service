02c116713cd4004f792a0ef2ca4f75dc473156ee
volskyi-dmytro <volskyi.dmytro@gmail.com>
2024-03-04 23:29:57 +0200

    Added some basic resilience logic

--- src/main/java/com/uabookshop/orderservice/book/BookClient.java
+++ src/main/java/com/uabookshop/orderservice/book/BookClient.java
@@ -2,7 +2,11 @@ package com.uabookshop.orderservice.book;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 import reactor.core.publisher.Mono;
+import reactor.util.retry.Retry;
+
+import java.time.Duration;
 
 @Component
 public class BookClient {
@@ -18,6 +22,11 @@ public class BookClient {
                 .get()
                 .uri(BOOKS_ROOT_API + isbn)
                 .retrieve()
-                .bodyToMono(Book.class);
+                .bodyToMono(Book.class)
+                .timeout(Duration.ofSeconds(3), Mono.empty())
+                .onErrorResume(WebClientResponseException.NotFound.class,
+                        exception -> Mono.empty())
+                .retryWhen(Retry.backoff(3, Duration.ofMillis(100)))
+                .onErrorResume(Exception.class, exception -> Mono.empty());
     }
 }
